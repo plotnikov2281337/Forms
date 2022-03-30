3e7a83b10b985c99167eeb40b8c3d4f5fd91a562
Виталий Сухих <suhih@VSPC34.RU>
2022-03-30 13:38:54 +0300

    1

--- WindowsFormsApp11/Form1.cs
+++ WindowsFormsApp11/Form1.cs
@@ -21,10 +21,6 @@ namespace WindowsFormsApp11
         {
             //1
         }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
+                
     }
 }
