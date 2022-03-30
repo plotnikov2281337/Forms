172b672d1d7934af16f8b231f06bd303dcd236bd
user <user@C1012>
2022-03-30 15:24:54 +0300

    1

--- WindowsFormsApp11/Form1.Designer.cs
+++ WindowsFormsApp11/Form1.Designer.cs
@@ -44,15 +44,22 @@
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // addition
             // 
-            this.addition.Location = new System.Drawing.Point(359, 32);
+            this.addition.Location = new System.Drawing.Point(357, 146);
             this.addition.Name = "addition";
             this.addition.Size = new System.Drawing.Size(75, 23);
             this.addition.TabIndex = 0;
@@ -62,7 +69,7 @@
             // 
             // subtraction
             // 
-            this.subtraction.Location = new System.Drawing.Point(359, 102);
+            this.subtraction.Location = new System.Drawing.Point(357, 208);
             this.subtraction.Name = "subtraction";
             this.subtraction.Size = new System.Drawing.Size(75, 23);
             this.subtraction.TabIndex = 1;
@@ -72,13 +79,16 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column7,
             this.Column8,
             this.Column9});
-            this.dataGridView1.Location = new System.Drawing.Point(236, 188);
+            this.dataGridView1.Location = new System.Drawing.Point(219, 237);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(344, 150);
             this.dataGridView1.TabIndex = 2;
             // 
@@ -104,7 +114,7 @@
             this.Column1,
             this.Column2,
             this.Column3});
-            this.dataGridView2.Location = new System.Drawing.Point(8, 12);
+            this.dataGridView2.Location = new System.Drawing.Point(6, 81);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.Size = new System.Drawing.Size(345, 150);
             this.dataGridView2.TabIndex = 3;
@@ -131,7 +141,7 @@
             this.Column4,
             this.Column5,
             this.Column6});
-            this.dataGridView3.Location = new System.Drawing.Point(440, 12);
+            this.dataGridView3.Location = new System.Drawing.Point(438, 81);
             this.dataGridView3.Name = "dataGridView3";
             this.dataGridView3.Size = new System.Drawing.Size(352, 150);
             this.dataGridView3.TabIndex = 4;
@@ -155,22 +165,81 @@
             // 
             this.errorProvider1.ContainerControl = this;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(112, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dataGridView2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.subtraction);
+            this.groupBox1.Controls.Add(this.addition);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.dataGridView3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(822, 419);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "высота";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "ширина";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(276, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Сгенерировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(859, 447);
-            this.Controls.Add(this.dataGridView3);
-            this.Controls.Add(this.dataGridView2);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.subtraction);
-            this.Controls.Add(this.addition);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -192,6 +261,12 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
         private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WindowsFormsApp11/Form1.cs
+++ WindowsFormsApp11/Form1.cs
@@ -12,23 +12,35 @@ namespace WindowsFormsApp11
 {
     public partial class Form1 : Form
     {
+        private int x, y;
         public Form1()
         {
             InitializeComponent();
-            int[,] matrix = new int [4, 4];
-            matrix[0, 0] =int.Parse(dataGridView1[0,0].Value.ToString());
-            InitDataGrid(dataGridView2, 4, 4);
+            /*int[,] matrix = new int [4, 4];*/
+            //matrix[0, 0] =int.Parse(dataGridView1[0,0].Value.ToString());
+            
+ 
+            
         }
 
         private void addition_Click(object sender, EventArgs e)
         {
-
+           
 
         }
 
         private void subtraction_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            x = int.Parse(textBox1.Text);
+            y = int.Parse(textBox2.Text);
+            InitDataGrid(dataGridView2, x, y);
+            InitDataGrid(dataGridView3, x, y);
+            InitDataGrid(dataGridView1, x, y);
         }
 
         private void InitDataGrid(DataGridView data, int x, int y)
@@ -42,14 +54,11 @@ namespace WindowsFormsApp11
                 for ( int j = 0; j < y; j++)
                 {
                     data.Columns[j].Width = 50;
-
+                    data.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
                 }
             }
             
-
-            
         }
-
 
     }
 }
