b513a028b3b4a9c5606748d88057eaa51ee769f6
Виталий Сухих <suhih@VSPC34.RU>
2022-03-31 13:04:08 +0300

    1

---
+++ WindowsFormsApp11/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp11
+{
+    internal class Controller
+    {
+        /*private Model model;
+        public void CalculateSumm(int[,] matrix1, int[,] matrix2)
+        {
+            model.sum(matrix1, matrix2);
+        }*/
+    }
+}
